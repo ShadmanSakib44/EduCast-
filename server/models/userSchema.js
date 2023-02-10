e86d666f4f2ff80c60b15bdda311e6451ee099b2
ShadmanSakib44 <shadmansakib20@iut-dhaka.edu>
2023-02-10 18:09:29 +0600

    before jwt token

--- server/models/userSchema.js
+++ server/models/userSchema.js
@@ -1,4 +1,10 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const keysecret = process.env.SECRET_KEY
+
 
 const userSchema = new mongoose.Schema({
     fname: {
@@ -36,7 +42,42 @@ const userSchema = new mongoose.Schema({
     ]
 });
 
+
+
+
+
+// hash password
+
+userSchema.pre("save", async function (next) {
+
+    if (this.isModified("password")) {
+        this.password = await bcrypt.hash(this.password, 12);
+        this.cpassword = await bcrypt.hash(this.cpassword, 12);
+    }
+    next()
+});
+
+
+// token generate
+userSchema.methods.generateAuthtoken = async function () {
+    try {
+        let token23 = jwt.sign({ _id: this._id }, keysecret, {
+            expiresIn: "1d"
+        });
+
+        this.tokens = this.tokens.concat({ token: token23 });
+        await this.save();
+        return token23;
+    } catch (error) {
+        res.status(422).json(error)
+    }
+}
+
+
 // createing model
 const userdb = new mongoose.model("users", userSchema);
 
 module.exports = userdb;
+
+
+// if (this.isModified("password")) {    }
