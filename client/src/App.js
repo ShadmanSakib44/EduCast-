78d236493fb5ab318403b9e057f1b58a28d2653c
oishy28 <95131852+oishy28@users.noreply.github.com>
2023-02-10 02:38:04 -0800

    register

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import Header from "./components/Header";
 import Login from "./components/Login";
+import Register from "./components/Register";
+import {Routes,Route} from "react-router-dom"
 
 function App() {
   return (
     <>
    <Header/>
-   <Login/>
+   <Routes>
+   <Route path="/" element={<Login/>}/>
+    <Route path="/register" element={<Register/>}/>
+   </Routes>
+   
    </>
   );
 }
