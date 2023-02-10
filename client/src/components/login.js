f52571e4b6a898d364f5c03a04ce0ae0fa389e53
oishy28 <95131852+oishy28@users.noreply.github.com>
2023-02-10 01:47:44 -0800

    form login

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -6,8 +6,29 @@ const Login = () => {
     <>
         <section>
             <div className='form_data'>
+                <div className='form_heading'>
                 <h1>Welcome Back, Log In</h1>
                 <p>Hi, We are glad you are back. Please login.</p>
+            </div>
+                <form>
+                    <div className='form_input'>
+                        <label htmlFor='email'>Email</label>
+                        <input type="email" name='email' id='email' placeholder='Enter your email address'/>
+                    </div>
+                    <div className='form_input'>
+                        <label htmlFor='password'>Password</label>
+                        <div className='two'>
+                        <input type="password" name='email' id='password' placeholder='Enter your password'/>
+                        <div className='showpass'>
+                            Show
+                        </div>
+                        </div>
+                        
+                    </div>
+
+                    <button className='btn'>Login</button>
+                    <p>Don't have an account?Registration</p>
+                </form>
             </div>
         </section>
     </>
