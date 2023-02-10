c1d7f12f2d73533d7393ced3346a3e951abc7315
oishy28 <95131852+oishy28@users.noreply.github.com>
2023-02-10 01:35:46 -0800

    login continue

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,12 @@
 import Header from "./components/Header";
+import Login from "./components/Login";
 
 function App() {
   return (
+    <>
    <Header/>
+   <Login/>
+   </>
   );
 }
 
---
+++ client/src/components/login.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./mix.css";
+
+const Login = () => {
+  return (
+    <>
+        <section>
+            <div className='form_data'>
+                <h1>Welcome Back, Log In</h1>
+                <p>Hi, We are glad you are back. Please login.</p>
+            </div>
+        </section>
+    </>
+  )
+}
+
+export default Login
+
